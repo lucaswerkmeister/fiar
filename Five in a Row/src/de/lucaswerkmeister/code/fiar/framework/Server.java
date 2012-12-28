48ac477898aa89f7fa81d7c11e809320b8bd3b65
Lucas Werkmeister <mail@lucaswerkmeister.de>
2012-12-28 19:31:21 +0100

    Licensing. Everything is now under GNU GPL v3.

--- Five in a Row/src/de/lucaswerkmeister/code/fiar/framework/Server.java
+++ Five in a Row/src/de/lucaswerkmeister/code/fiar/framework/Server.java
@@ -1,3 +1,20 @@
+/*
+ * Five in a Row, a short game.
+ * Copyright (C) 2012/2013 Lucas Werkmeister
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.lucaswerkmeister.code.fiar.framework;
 
 import java.util.Set;
@@ -17,8 +34,8 @@ import de.lucaswerkmeister.code.fiar.framework.event.PlayerAction;
  * unspecified, but the {@link UnknownClientException} class is provided for
  * uniform behavior if servers wish to throw such an exception.
  * <p>
- * If, for any reason (e. g., the client „spams“ the server with requests), the
- * server decides to „fire” a client, the preferred way for it to do so is to
+ * If, for any reason (e.ï¿½g., the client ï¿½spamsï¿½ the server with requests), the
+ * server decides to ï¿½fireï¿½ a client, the preferred way for it to do so is to
  * throw a {@link ClientFiredException}.
  * 
  * @author Lucas Werkmeister
