b2be900b839d702ab2db564e98146b651a8ed2c6
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-01-07 11:23:38 +0100

    Added a new JUnit test for the FixedServer.

--- Five in a Row/test/FixedServerTest.java
+++ Five in a Row/test/FixedServerTest.java
@@ -9,17 +9,24 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
+import de.lucaswerkmeister.code.fiar.framework.Block;
+import de.lucaswerkmeister.code.fiar.framework.Board;
 import de.lucaswerkmeister.code.fiar.framework.Client;
+import de.lucaswerkmeister.code.fiar.framework.Joker;
 import de.lucaswerkmeister.code.fiar.framework.Player;
 import de.lucaswerkmeister.code.fiar.framework.Server;
 import de.lucaswerkmeister.code.fiar.framework.event.BlockDistributionAccepted;
+import de.lucaswerkmeister.code.fiar.framework.event.BlockField;
 import de.lucaswerkmeister.code.fiar.framework.event.BoardSizeProposal;
 import de.lucaswerkmeister.code.fiar.framework.event.GameEvent;
 import de.lucaswerkmeister.code.fiar.framework.event.JokerDistributionAccepted;
+import de.lucaswerkmeister.code.fiar.framework.event.JokerField;
 import de.lucaswerkmeister.code.fiar.framework.event.PhaseChange;
 import de.lucaswerkmeister.code.fiar.framework.event.PlaceStone;
 import de.lucaswerkmeister.code.fiar.framework.event.PlayerAction;
 import de.lucaswerkmeister.code.fiar.framework.event.PlayerVictory;
+import de.lucaswerkmeister.code.fiar.framework.event.UnblockField;
+import de.lucaswerkmeister.code.fiar.framework.event.UnjokerField;
 import de.lucaswerkmeister.code.fiar.framework.exception.IllegalMoveException;
 import de.lucaswerkmeister.code.fiar.servers.FixedServer;
 
@@ -41,7 +48,7 @@ public class FixedServerTest extends Client {
 			events = new LinkedList<>();
 			Player p1 = new Player("Player 1", Color.blue, 1);
 			Player p2 = new Player("Player 2", Color.cyan, 2);
-			FixedServer server = new FixedServer(new Client[] {this }, new Player[][] {new Player[] {p1, p2 } });
+			Server server = new FixedServer(new Client[] {this }, new Player[][] {new Player[] {p1, p2 } });
 
 			// Init: Board Size, Block Distribution, Joker Distribution
 			act(server, new BoardSizeProposal(p1, new Dimension(10, 10)));
@@ -64,8 +71,81 @@ public class FixedServerTest extends Client {
 			act(server, new PlaceStone(p1, 4, 1));
 			act(server, new PlaceStone(p2, 0, 1));
 			act(server, new PlaceStone(p1, 5, 1));
+
+			// Victory
 			Assert.assertTrue(PlayerVictory.class.isInstance(events.peek()));
-			Assert.assertEquals(p1, ((PlayerVictory) events.peek()).getWinningPlayer());
+			Assert.assertEquals(p1, ((PlayerVictory) events.poll()).getWinningPlayer());
+			Assert.assertNull(events.poll());
+		} catch (Throwable t) {
+			fail("Exception: " + t.toString());
+			if (t instanceof ThreadDeath)
+				throw (ThreadDeath) t;
+		}
+	}
+
+	/**
+	 * Tests advanced gameplay with blocked fields, jokers and three players. No illegal action attempts are made.
+	 */
+	@Test
+	public void testAdvanced() {
+		try {
+			events = new LinkedList<>();
+			Player p1 = new Player("Player 1", Color.blue, 1);
+			Player p2 = new Player("Player 2", Color.cyan, 2);
+			Player p3 = new Player("Player 3", Color.pink, 3);
+			Server server = new FixedServer(new Client[] {this }, new Player[][] {new Player[] {p1, p2, p3 } });
+
+			// Board size
+			act(server, new BoardSizeProposal(p1, new Dimension(10, 10)));
+			act(server, new BoardSizeProposal(p2, new Dimension(10, 10)));
+			act(server, new BoardSizeProposal(p3, new Dimension(10, 10)));
+			Assert.assertTrue(PhaseChange.class.isInstance(events.poll()));
+
+			// Block (3,5) and (3,6)
+			// To confuse the server, we accept the block distribution before we set the blocks.
+			// The server should be able to handle this.
+			Board targetBoard = server.getCurrentBoard(this);
+			targetBoard.setPlayerAt(3, 5, Block.getInstance());
+			targetBoard.setPlayerAt(3, 6, Block.getInstance());
+			act(server, new BlockDistributionAccepted(p1, targetBoard));
+			act(server, new BlockDistributionAccepted(p2, targetBoard));
+			act(server, new BlockDistributionAccepted(p3, targetBoard));
+			// Now set the blocks. We add and remove another block in between.
+			act(server, new BlockField(p1, 3, 5));
+			act(server, new BlockField(p1, 3, 7));
+			act(server, new BlockField(p1, 3, 6));
+			act(server, new UnblockField(p1, 3, 7));
+			Assert.assertTrue(PhaseChange.class.isInstance(events.poll()));
+
+			// Set (0,0) and (9,9) as joker fields. This is completely analogous to the blocking procedure.
+			targetBoard.setPlayerAt(0, 0, Joker.getInstance());
+			targetBoard.setPlayerAt(9, 9, Joker.getInstance());
+			act(server, new JokerDistributionAccepted(p1, targetBoard));
+			act(server, new JokerDistributionAccepted(p2, targetBoard));
+			act(server, new JokerDistributionAccepted(p3, targetBoard));
+			act(server, new JokerField(p1, 0, 0));
+			act(server, new JokerField(p1, 1, 7));
+			act(server, new JokerField(p1, 9, 9));
+			act(server, new UnjokerField(p1, 1, 7));
+			Assert.assertTrue(PhaseChange.class.isInstance(events.poll()));
+
+			// Game
+			act(server, new PlaceStone(p1, 1, 1));
+			act(server, new PlaceStone(p2, 8, 8));
+			act(server, new PlaceStone(p3, 0, 9));
+			act(server, new PlaceStone(p1, 2, 2));
+			act(server, new PlaceStone(p2, 7, 7));
+			act(server, new PlaceStone(p3, 1, 8));
+			act(server, new PlaceStone(p1, 3, 3));
+			act(server, new PlaceStone(p2, 6, 6));
+			act(server, new PlaceStone(p3, 4, 4));
+			act(server, new PlaceStone(p1, 2, 7));
+			act(server, new PlaceStone(p2, 5, 5));
+
+			// Victory
+			Assert.assertTrue(PlayerVictory.class.isInstance(events.peek()));
+			Assert.assertEquals(p2, ((PlayerVictory) events.poll()).getWinningPlayer());
+			Assert.assertNull(events.poll());
 		} catch (Throwable t) {
 			fail("Exception: " + t.toString());
 			if (t instanceof ThreadDeath)
